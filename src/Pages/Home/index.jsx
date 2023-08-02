cfed7b42c0a2432a3f2e3ff5a42ba482d3dc65cf
Christian Figueroa <christian-f79@hotmail.com>
2023-08-01 23:02:23 -0500

    Inserción de múltiples cards mediante consumo de FakeStore API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,28 @@
+import { useState, useEffect } from 'react' //Para guardar la información suministrada por la API. Con useEffect se busca el consumo de la API
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState(null)
   
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
 
     return (
       <Layout>
         Home
-        <Card />
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map(item => (
+              <Card key={item.id} data={item} />
+              ))
+          }
+        </div>
+        
+        
       </Layout>
     )
   }
