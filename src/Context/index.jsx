ac708a4eb12e460dcc4911b505b4e797293baacd
Christian Figueroa <christian-f79@hotmail.com>
2023-08-05 00:38:06 -0500

    Se agregan productos al carrito de compra a la vez que se despliega el cuadro de My Order

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,10 +5,10 @@ export const ShoppingCartContext = createContext()
 export const ShoppingCartProvider = ({children}) => {
     
     //Shopping Cart - Incrementar cantidad
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0) //Inicializa el estado en cero para que la cantidad de elementos adquiridos empiece en cero
 
-    //Product Detail - Despliegue de detalle de producto
-    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    //Product Detail - Despliegue y cierre de detalle de producto
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false) //Inicializa el estado en false para que el detalle del producto esté inicialmente cerrado 
 /*    
 parámetro: Lectura
 setParámetro: Modificación
@@ -16,8 +16,17 @@ setParámetro: Modificación
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
 
+    //Checkout Side Menu - Despliegue y cierre de detalle Mi orden
+    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false) //Inicializa el estado en false para que el detalle del producto esté inicialmente cerrado 
+
+    const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+    const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+
     //Product Detail - Mostrar producto
-    const [productToShow, setProductToShow] = useState({})
+    const [productToShow, setProductToShow] = useState({}) //Inicializa el estado con un objeto vació para contener todas las especificaciones del producto
+
+    //Shopping cart - Adicionar productos al carrito
+    const [cartProducts, setCartProducts] = useState([]) //Inicializa el estado con un array vacío para contener las diferentes cantidades de la compra
 
     return (
         <ShoppingCartContext.Provider value={{
@@ -27,7 +36,12 @@ setParámetro: Modificación
             closeProductDetail,
             isProductDetailOpen,
             productToShow,
-            setProductToShow
+            setProductToShow,
+            cartProducts,
+            setCartProducts,
+            isCheckoutSideMenuOpen,
+            openCheckoutSideMenu,
+            closeCheckoutSideMenu
         }}>
             {children}
         </ShoppingCartContext.Provider>
