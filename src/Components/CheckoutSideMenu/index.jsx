922292379a8c7f89b3d41ad591e63ed75accadae
Christian Figueroa <christian-f79@hotmail.com>
2023-08-05 21:35:11 -0500

    Se agregan los productos al carrito de compra y se visualizan los detalles de cada producto en el cuadro de My Order. Se bloquean compras de productos duplicados

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,14 +2,14 @@ import { useContext } from 'react'
 import { XCircleIcon } from '@heroicons/react/24/solid'
 import './styles.css'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../OrderCard'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
-    
+    console.log('CART: ', context.cartProducts)
 
     return (
-        <aside 
-        className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+        <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>My Order</h2>
                 <div>
@@ -18,7 +18,18 @@ const CheckoutSideMenu = () => {
 
                 </div>
             </div>
-                        
+            <div className='px-6 overflow-y-scroll'>
+                {
+                    context.cartProducts.map(product => (
+                        <OrderCard
+                        key={product.id} 
+                        title={product.title}
+                        imageUrl={product.images}
+                        price={product.price}
+                        />
+                    ))
+                }
+            </div>            
         </aside>
     )
 }
