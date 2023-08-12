1955bc31890e41ecdf830e083904cf8ca5bb7986
Christian Figueroa <christian-f79@hotmail.com>
2023-08-12 17:43:14 -0500

    Lista de órdenes de compra

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,5 +1,8 @@
 
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+
 import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 import OrderCard from '../../Components/OrderCard'
@@ -10,7 +13,12 @@ function MyOrder() {
 
     return (
       <Layout>
-        MyOrder
+        <div className='flex items-center justify-center relative w-80'>
+          <Link to='/my-orders' className='absolute left-0'>
+            <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer" />
+          </Link>
+          <h1>My Order</h1>
+        </div>
         <div className='flex flex-col w-80'>
                 {
                     context.order?.slice(-1)[0].products.map(product => (
